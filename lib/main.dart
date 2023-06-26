b3bcd26cd4e0f3493649067ce123348872b2992a
Raj Singh <terahero554@gmail.com>
2023-06-26 22:52:32 +0530

    navigation

--- lib/main.dart
+++ lib/main.dart
@@ -23,6 +23,7 @@ class MyApp extends StatelessWidget {
       ),
       home:const SplashScreen(),
     );
+    
   }
 }
 
--- lib/views/splash_screen/splash_screen.dart
+++ lib/views/splash_screen/splash_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:ecommerce_app/consts/consts.dart';
+import 'package:ecommerce_app/views/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 
@@ -10,26 +13,43 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  
+
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Pallete.backgroundColor,
-      body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Lottie.asset('assets/animations/cart_animation.json',width: 300,height: 300),
-          appname.text.fontFamily(bold).size(22).white.make(),
-          5.heightBox,
-          appversion.text.white.make(),
-          // const Spacer(),
-          const Align(
-            alignment: Alignment.bottomCenter,
-            child: Text(credits, style: TextStyle(color: Pallete.whiteColor, fontSize: 14,fontFamily: semibold))),
-          20.heightBox,
-          
-        ],
-      )),
-    );
+  void initState() {
+    super.initState();
+
+    Timer(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (_) => const HomeScreen()));
+    });
   }
-}
+
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        backgroundColor: Pallete.backgroundColor,
+        body: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Lottie.asset('assets/animations/cart_animation.json',
+                width: 300, height: 300),
+            appname.text.fontFamily(bold).size(22).white.make(),
+            5.heightBox,
+            appversion.text.white.make(),
+            // const Spacer(),
+            const Align(
+                alignment: Alignment.bottomCenter,
+                child: Text(credits,
+                    style: TextStyle(
+                        color: Pallete.whiteColor,
+                        fontSize: 14,
+                        fontFamily: semibold))),
+            20.heightBox,
+          ],
+        )),
+      );
+    }
+  }
+
